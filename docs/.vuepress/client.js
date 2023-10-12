e0664a5aeb251e4a2323a64db047d5fb3c5afc5f
wzCoding <1429354968@qq.com>
2023-10-12 22:48:34 +0800

    submit the code

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,4 +1,6 @@
 import { defineClientConfig } from '@vuepress/client'
+import { Timer } from './public/util/utils'
+import { onMounted } from 'vue'
 import Mcard from './components/Mcard.vue'
 import Minfo from './components/Minfo.vue'
 
@@ -12,6 +14,10 @@ export default defineClientConfig({
     //注册自定义组件
     componentList.forEach(c => {
       app.component(c.name, c.component)
+    })
+
+    onMounted(() => {
+        console.log("onMounted")
     })
   }
 })
