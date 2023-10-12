9a021fdaec3ce4d0d7c8d47f50c9cc67262d537e
wzCoding <1429354968@qq.com>
2023-10-12 23:45:29 +0800

    submit the code

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -18,38 +18,38 @@ export default defineClientConfig({
   },
   setup() {
     onMounted(() => {
-      
-      const width = window.innerWidth,height = window.innerHeight;
+
+      const width = window.innerWidth, height = window.innerHeight;
       const bgContainer = document.getElementsByClassName("vp-blog-mask");
       const myCanvas = document.createElement("canvas");
+      const ctx = myCanvas.getContext("2d");
       myCanvas.width = width;
       myCanvas.height = height;
-      //myCanvas.style.backgroundColor = "#000000";
+      myCanvas.style.backgroundColor = "#000000";
 
       bgContainer[0].append(myCanvas);
 
       const fontSize = 16;
       const fontWeight = 700;
       const fontFamily = "微软雅黑";
-      const ctx = myCanvas.getContext("2d");
 
       const letters = Array(Math.ceil(width / fontSize)).fill(0);
       const text = "wzCoding".split("");
 
-      const codeRain = function() {
-        ctx.fillStyle = "rgba(0,0,0,0.5)";
+      const codeRain = function () {
+        ctx.fillStyle = "rgba(0,0,0,0.1)";
         ctx.fillRect(0, 0, width, height);
         ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
         ctx.fillStyle = "#0f0";
 
-        letters.forEach(function(item, index) {
+        letters.forEach(function (item, index) {
           ctx.fillText(text[Math.floor(Math.random() * text.length)], index * fontSize, item + fontSize);
           letters[index] = item >= height || item > 8888 * Math.random() ? 0 : item + fontSize;
         })
       }
 
       const timer = new Timer();
-      timer.interval(codeRain,100)
+      timer.interval(codeRain, 60)
     })
   }
 })
