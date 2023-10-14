f9f493e0dd9e4a7115a73de2e192b946e8de479a
wzCoding <1429354968@qq.com>
2023-10-14 23:41:56 +0800

    submit the code

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,7 +1,9 @@
 import { defineClientConfig } from '@vuepress/client'
-import { Timer } from './public/util/timer'
-import { createCanvas } from './public/util/canvas'
 import { onMounted } from 'vue'
+
+import { createCanvas } from './public/util/canvas'
+import { Rain } from './public/util/rain'
+
 import Mcard from './components/Mcard.vue'
 import Minfo from './components/Minfo.vue'
 
@@ -25,36 +27,15 @@ export default defineClientConfig({
         const theme = html[0].getAttribute("data-theme");
         console.log(theme)
       })
-
-      const width = window.innerWidth, height = window.innerHeight;
-      const myCanvas = new createCanvas("vp-blog-mask", {
+      const myCanvas = createCanvas("vp-blog-mask", {
         backgroundColor: "#000000",
-        width,
-        height,
+        width:window.innerWidth,
+        height:window.innerHeight,
       });
-      const ctx = myCanvas.getContext("2d");
 
-      const fontSize = 16;
-      const fontWeight = 700;
-      const fontFamily = "微软雅黑";
-
-      const letters = Array(Math.ceil(width / fontSize)).fill(0);
-      const text = "wzCoding".split("");
-
-      const codeRain = function () {
-        ctx.fillStyle = "rgba(0,0,0,0.08)";
-        ctx.fillRect(0, 0, width, height);
-        ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
-        ctx.fillStyle = "#0f0";
-
-        letters.forEach(function (item, index) {
-          ctx.fillText(text[Math.floor(Math.random() * text.length)], index * fontSize, item + fontSize);
-          letters[index] = item >= height || item > 8888 * Math.random() ? 0 : item + fontSize;
-        })
-      }
-
-      const timer = new Timer();
-      timer.interval(codeRain, 60)
+      const rain = new Rain(myCanvas);
+      rain.start("wzCoding",60);
+      
     })
   }
 })
--- docs/.vuepress/public/util/rain.js
+++ docs/.vuepress/public/util/rain.js
@@ -1,31 +1,46 @@
 import { Timer } from "./timer";
 
-const width = window.innerWidth, height = window.innerHeight;
-const myCanvas = new createCanvas("vp-blog-mask", {
-  backgroundColor: "#000000",
-  width,
-  height,
-});
-const ctx = myCanvas.getContext("2d");
+let cvs = null;
+let ctx = null;
 
-const fontSize = 16;
-const fontWeight = 700;
-const fontFamily = "微软雅黑";
+class Rain {
+    constructor(canvas) {
+        this.fontSize = 16;
+        this.fontWeight = 700;
+        this.fontFamily = "微软雅黑";
 
-const letters = Array(Math.ceil(width / fontSize)).fill(0);
-const text = "wzCoding".split("");
+        this.width = canvas.width;
+        this.height = canvas.height;
 
-const codeRain = function () {
-  ctx.fillStyle = "rgba(0,0,0,0.08)";
-  ctx.fillRect(0, 0, width, height);
-  ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
-  ctx.fillStyle = "#0f0";
+        this.letters = Array(Math.ceil(this.width / this.fontSize)).fill(0);
 
-  letters.forEach(function (item, index) {
-    ctx.fillText(text[Math.floor(Math.random() * text.length)], index * fontSize, item + fontSize);
-    letters[index] = item >= height || item > 8888 * Math.random() ? 0 : item + fontSize;
-  })
+        cvs = canvas;
+        ctx = canvas.getContext("2d");
+    }
+    createRain(text) {
+
+        const textList = text.length ? text.split("") : "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");;
+
+        ctx.fillStyle = "rgba(0,0,0,0.08)";
+        ctx.fillRect(0, 0, this.width, this.height);
+        ctx.font = `${this.fontWeight} ${this.fontSize}px ${this.fontFamily}`;
+        ctx.fillStyle = "#0f0";
+
+        this.letters.forEach((item, index) => {
+            ctx.fillText(textList[Math.floor(Math.random() * textList.length)], index * this.fontSize, item + this.fontSize);
+            this.letters[index] = item >= this.height || item > 8888 * Math.random() ? 0 : item + this.fontSize;
+        })
+    }
+    start(text, speed) {
+        const timer = new Timer();
+        timer.interval(() => {
+            this.createRain(text);
+        }, speed);
+    }
 }
 
-const timer = new Timer();
-timer.interval(codeRain, 60)
+export {
+    Rain
+}
+
+
