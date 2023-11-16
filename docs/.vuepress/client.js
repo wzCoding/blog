fc2a263c38e54bdd6e36b55e8f8aaadaa25a7255
wzCoding <48940636+wzCoding@users.noreply.github.com>
2023-11-16 18:19:59 +0800

    更新 client.js
    submit the code

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -22,63 +22,63 @@ export default defineClientConfig({
       app.component(c.name, c.component)
     })
   },
-  // setup() {
-  //   const themes = {
-  //     "light": {
-  //       canvas: null,
-  //       instance: null
-  //     },
-  //     "dark": {
-  //       canvas: null,
-  //       instance: null
-  //     }
-  //   }
-  //   const route = useRoute();
-  //   const theme = ref("light");
+  setup() {
+   const themes = {
+    "light": {
+     canvas: null,
+      instance: null
+       },
+    "dark": {
+       canvas: null,
+       instance: null
+    }
+  }
+    const route = useRoute();
+  const theme = ref("light");
 
-  //   onMounted(() => {
-  //     for (let key in themes) {
-  //       themes[key].canvas = new Canvas({
-  //         container: document.body,
-  //         id: `${key}-canvas`,
-  //         width: document.documentElement.clientWidth,
-  //         height: document.documentElement.clientHeight,
-  //         styles: {
-  //           background: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
-  //           position: "absolute",
-  //           inset: 0
-  //         }
-  //       })
-  //       if (key == "light") {
-  //         themes[key].instance = new Sea(themes[key].canvas);
-  //         themes[key].instance.addSun(sun);
-  //         themes[key].instance.addWave(waves);
-  //         themes[key].instance.addCloud(clouds);
-  //       } else {
-  //         themes[key].instance = new Rain(themes[key].canvas, "wzCoding");
-  //       }
-  //     }
+   onMounted(() => {
+     for (let key in themes) {
+     themes[key].canvas = new Canvas({
+       container: document.body,
+         id: `${key}-canvas`,
+        width: document.documentElement.clientWidth,
+         height: document.documentElement.clientHeight,
+       styles: {
+            background: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
+           position: "absolute",
+           inset: 0
+         }
+       })
+      if (key == "light") {
+        themes[key].instance = new Sea(themes[key].canvas);
+         themes[key].instance.addSun(sun);
+         themes[key].instance.addWave(waves);
+        themes[key].instance.addCloud(clouds);
+       } else {
+         themes[key].instance = new Rain(themes[key].canvas, "wzCoding");
+       }
+      }
 
-  //     const changeCanvas = function () {
-  //       if (route.fullPath !== "/") {
-  //         themes[theme.value].instance.stop();
-  //       } else {
-  //         for (let t in themes) {
-  //           themes[t].instance.stop();
-  //         }
-  //         themes[theme.value].instance.start(60);
-  //       }
-  //     }
+     const changeCanvas = function () {
+        if (route.fullPath !== "/") {
+         themes[theme.value].instance.stop();
+        } else {
+         for (let t in themes) {
+            themes[t].instance.stop();
+          }
+         themes[theme.value].instance.start(60);
+        }
+       }
 
-  //     观察器
-  //     const observer = new MutationObserver((list) => {
-  //       theme.value = list[0].target.getAttribute("data-theme");
-  //     });
-  //     observer.observe(document.documentElement, { attributes: true });
+     观察器
+       const observer = new MutationObserver((list) => {
+       theme.value = list[0].target.getAttribute("data-theme");
+     });
+       observer.observe(document.documentElement, { attributes: true });
 
-  //     watchEffect(() => {
-  //       changeCanvas();
-  //     });
-  //   });
-  // }
+     watchEffect(() => {
+        changeCanvas();
+      });
+    });
+  }
 })
