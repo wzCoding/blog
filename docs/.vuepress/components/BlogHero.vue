e7c8438b169c1fa1a89bf12d98f49c575c10410a
wzCoding <1429354968@qq.com>
2023-11-21 22:26:04 +0800

    submit the code

--- docs/.vuepress/components/BlogHero.vue
+++ docs/.vuepress/components/BlogHero.vue
@@ -5,64 +5,61 @@
     </div>
 </template>
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, computed } from 'vue'
 import { usePageFrontmatter } from '@vuepress/client'
 import { myCanvas } from './canvas/canvas'
 import { Rain } from './canvas/rain'
 import { Sea } from './canvas/sea'
 import { waves, clouds, sun } from './canvas/data'
 const defaultTheme = "light";
-const show = ref(true);
+const theme = ref(defaultTheme);
+const show = computed(() => {
+    return theme.value.value == defaultTheme
+});
+
 const hero = ref();
 const themes = {};
-
 function stopCanvas() {
-    console.log("stop")
     for (const key in themes) {
         themes[key].stop();
     }
 }
-function startCanvas(theme) {
-    console.log("start")
-    themes[theme].start(60);
+function startCanvas() {
+    themes[theme.value].start(60);
 }
-function initCanvas(theme) {
-    if (!themes[theme]) {
-        const canvas = new myCanvas({
+
+const info = usePageFrontmatter()
+onMounted(() => {
+    const canvas = computed(() => {
+        return new myCanvas({
             parent: hero.value,
-            id: theme,
+            id: theme.value,
             styles: {
                 background: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
                 position: "absolute",
                 inset: 0
             }
         })
-        if (theme == defaultTheme) {
-            const sea = new Sea(canvas);
-            sea.addWave(waves);
-            sea.addSun(sun);
-            sea.addCloud(clouds);
-            themes[theme] = sea;
-        } else {
-            themes[theme] = new Rain(canvas, "wzCoding");
-        }
-    }
-    stopCanvas();
-    startCanvas(theme);
-}
+    });
+    console.log(canvas.value)
+    const rain = new Rain(canvas.value, "wzCoding");
+    const sea = new Sea(canvas.value);
+    sea.addWave(waves);
+    sea.addSun(sun);
+    sea.addCloud(clouds);
 
-const info = usePageFrontmatter()
-onMounted(() => {
     const observer = new MutationObserver((list) => {
-        const currentTheme = list[0].target.getAttribute("data-theme");
-        show.value = currentTheme == defaultTheme;
-        console.log(111)
-        //initCanvas(currentTheme);
+        theme.value = list[0].target.getAttribute("data-theme");
+        if (!themes[theme.value]) {
+            themes[theme.value] = theme.value == defaultTheme ? sea : rain;
+        }
+        stopCanvas();
+        startCanvas();
     });
     observer.observe(document.documentElement, { attributes: true });
 })
-onUnmounted(()=>{
-    stopCanvas()
+onUnmounted(() => {
+    stopCanvas();
 })
 </script>
 <style lang="scss">
