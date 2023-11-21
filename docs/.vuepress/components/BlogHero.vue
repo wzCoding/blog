7c3a69250e626548c56e45f81135930dd731200d
wzCoding <1429354968@qq.com>
2023-11-21 23:06:58 +0800

    submit the code

--- docs/.vuepress/components/BlogHero.vue
+++ docs/.vuepress/components/BlogHero.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="hero-mask" ref="hero">
-        <canvas id="light" v-show="show"></canvas>
-        <canvas id="dark" v-show="!show"></canvas>
+        <canvas :id="canvasId"></canvas>
     </div>
 </template>
 <script setup>
-import { ref, onMounted, onUnmounted, computed } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { usePageFrontmatter } from '@vuepress/client'
 import { myCanvas } from './canvas/canvas'
 import { Rain } from './canvas/rain'
@@ -13,10 +12,6 @@ import { Sea } from './canvas/sea'
 import { waves, clouds, sun } from './canvas/data'
 const defaultTheme = "light";
 const theme = ref(defaultTheme);
-const show = computed(() => {
-    return theme.value.value == defaultTheme
-});
-
 const hero = ref();
 const themes = {};
 function stopCanvas() {
@@ -27,23 +22,20 @@ function stopCanvas() {
 function startCanvas() {
     themes[theme.value].start(60);
 }
-
+const canvasId = "theme-canvas";
 const info = usePageFrontmatter()
 onMounted(() => {
-    const canvas = computed(() => {
-        return new myCanvas({
-            parent: hero.value,
-            id: theme.value,
-            styles: {
-                background: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
-                position: "absolute",
-                inset: 0
-            }
-        })
-    });
-    console.log(canvas.value)
-    const rain = new Rain(canvas.value, "wzCoding");
-    const sea = new Sea(canvas.value);
+    const canvas = new myCanvas({
+        parent: hero.value,
+        id: canvasId,
+        styles: {
+            background: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
+            position: "absolute",
+            inset: 0
+        }
+    })
+    const rain = new Rain(canvas, "wzCoding");
+    const sea = new Sea(canvas);
     sea.addWave(waves);
     sea.addSun(sun);
     sea.addCloud(clouds);
