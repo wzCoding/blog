e4a7fc7a6f758fc034878153f8a98d33859fcb12
wzCoding <1429354968@qq.com>
2023-11-26 17:50:18 +0800

    blog首页修改

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,25 +1,18 @@
 import { defineClientConfig } from '@vuepress/client'
 import { onMounted, ref, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
-import { createPinia, defineStore } from 'pinia'
 import { myCanvas } from './canvas/canvas'
 import { Rain } from './canvas/rain'
 import { Sea } from './canvas/sea'
-import { clouds, waves, sun } from './canvas/data'
+import { waves } from './canvas/data'
 import Mcard from './components/Mcard.vue'
 import Minfo from './components/Minfo.vue'
 const components = [
   { name: 'Mcard', component: Mcard },
   { name: 'Minfo', component: Minfo },
 ]
-export const useThemeStore = defineStore('theme', () => {
-  const theme = ref("light");
-  return { theme }
-})
 export default defineClientConfig({
   enhance({ app }) {
-    const pinia = createPinia()
-    app.use(pinia)
     components.forEach(c => {
       app.component(c.name, c.component);
     });
@@ -36,8 +29,8 @@ export default defineClientConfig({
         styles: {
           background: "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)",
           position: "absolute",
-          top:`${navHeight}px`,
-          left:0,
+          top: `${navHeight}px`,
+          left: 0,
           transition: "all 0.3s"
         }
       }
@@ -45,25 +38,25 @@ export default defineClientConfig({
       const rain = new Rain(canvas);
       const sea = new Sea(canvas);
       sea.addWave(waves);
-      
+
       const themes = {
         "light": sea,
         "dark": rain
       }
+      const theme = ref();
       const route = useRoute();
-      const themeStore = useThemeStore();
-      themeStore.theme = window.localStorage.getItem("vuepress-theme-hope-scheme");
+      theme.value = window.localStorage.getItem("vuepress-theme-hope-scheme");
       const observer = new MutationObserver((list) => {
-        themeStore.theme = list[0].target.getAttribute("data-theme");
+        theme.value = list[0].target.getAttribute("data-theme");
       });
       const animate = () => {
         if (route.fullPath !== "/") {
-          themes[themeStore.theme].stop();
+          themes[theme.value].stop();
           observer.disconnect();
         } else {
           observer.observe(document.documentElement, { attributes: true });
           for (let key in themes) { themes[key].stop() }
-          themes[themeStore.theme].start(60);
+          themes[theme.value].start(60);
         }
       }
       //使用watchEffect立即执行canvas动画并监听
--- docs/README.md
+++ docs/README.md
@@ -6,8 +6,7 @@ title: 主页
 heroImage: /head.png
 heroText: wzCoding
 heroFullScreen: true
-tagline: 心有所向，日复一日，必有精进
-taglineDark: 身无彩凤双飞翼，心有灵犀一点通
+tagline: 身无彩凤双飞翼，心有灵犀一点通
 actions:
   - text: 开始探索 →
     link: ./blog-html/
