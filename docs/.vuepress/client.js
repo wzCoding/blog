a910695c7f36b0bad9c7809e3bea58b1b927583d
wzCoding <1429354968@qq.com>
2023-11-27 18:22:33 +0800

    submit the code

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -18,52 +18,52 @@ export default defineClientConfig({
     });
   },
   setup() {
-    onMounted(() => {
-      const navHeight = document.querySelector("#navbar").clientHeight;
-      const option = {
-        parent: document.body,
-        id: `theme-canvas`,
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight - navHeight,
-        styles: {
-          background: "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)",
-          position: "absolute",
-          inset: 0,
-          top:`${navHeight}px`,
-          transition: "all 0.3s"
-        }
-      }
-      const canvas = new myCanvas(option);
-      const rain = new Rain(canvas);
-      const sea = new Sea(canvas);
-      sea.addWave(waves);
+    // onMounted(() => {
+    //   const navHeight = document.querySelector("#navbar").clientHeight;
+    //   const option = {
+    //     parent: document.body,
+    //     id: `theme-canvas`,
+    //     width: document.documentElement.clientWidth,
+    //     height: document.documentElement.clientHeight - navHeight,
+    //     styles: {
+    //       background: "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)",
+    //       position: "absolute",
+    //       inset: 0,
+    //       top:`${navHeight}px`,
+    //       transition: "all 0.3s"
+    //     }
+    //   }
+    //   const canvas = new myCanvas(option);
+    //   const rain = new Rain(canvas);
+    //   const sea = new Sea(canvas);
+    //   sea.addWave(waves);
 
-      const themes = {
-        "light": sea,
-        "dark": rain
-      }
-      const theme = ref("light");
-      const route = useRoute();
-      const cacheTheme = window.localStorage.getItem("vuepress-theme-hope-scheme");
-      theme.value = cacheTheme && cacheTheme !== "auto" ? cacheTheme : "light";
-      const observer = new MutationObserver((list) => {
-        theme.value = list[0].target.getAttribute("data-theme");
-      });
-      const animate = () => {
-        if (route.fullPath !== "/") {
-          themes[theme.value].stop();
-          observer.disconnect();
-        } else {
-          observer.observe(document.documentElement, { attributes: true });
-          for (let key in themes) { themes[key].stop() }
-          themes[theme.value].start(60);
-        }
-      }
-      //使用watchEffect立即执行canvas动画并监听
-      watchEffect(() => {
-        animate();
-      })
-    });
+    //   const themes = {
+    //     "light": sea,
+    //     "dark": rain
+    //   }
+    //   const theme = ref("light");
+    //   const route = useRoute();
+    //   const cacheTheme = window.localStorage.getItem("vuepress-theme-hope-scheme");
+    //   theme.value = cacheTheme && cacheTheme !== "auto" ? cacheTheme : "light";
+    //   const observer = new MutationObserver((list) => {
+    //     theme.value = list[0].target.getAttribute("data-theme");
+    //   });
+    //   const animate = () => {
+    //     if (route.fullPath !== "/") {
+    //       themes[theme.value].stop();
+    //       observer.disconnect();
+    //     } else {
+    //       observer.observe(document.documentElement, { attributes: true });
+    //       for (let key in themes) { themes[key].stop() }
+    //       themes[theme.value].start(60);
+    //     }
+    //   }
+    //   //使用watchEffect立即执行canvas动画并监听
+    //   watchEffect(() => {
+    //     animate();
+    //   })
+    // });
   }
 })
 
